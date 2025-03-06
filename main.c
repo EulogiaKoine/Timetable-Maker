b0aec01c97926eca4cab94be68d575c7374b6652
지웅 강 <poding657@gmail.com>
2025-03-06 13:40:01 +0900

    FIX : 매개변수 수정

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
-#include "scheduleViewer.h"
+﻿#include "scheduleViewer.h"
 #include "interface_function.h"
 
-int main(int argc, char* args){
+int main(int argc, char* args) {
     // 프로그램 핸들
     HINSTANCE hInst = GetModuleHandle(NULL);
-    HINSTANCE hInstance = GetModuleHandle(NULL);
-    
-    showWindow1(hInstance);
+
+    showWindow1(hInst);
     showWindow2(hInst);
     return 0;
 }
